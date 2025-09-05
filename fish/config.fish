a7e6a0be8b0fcad970218b0dfc093328297b383d
8charlie <charlie.j.armstrong@protonmail.com>
2025-09-05 12:19:34 +0100

    a

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	function see
-		builtin cd $argv[1]; and timeout 1s ls -l
+		builtin cd $argv[1]; and timeout 1s ls
 	end
 end
 
